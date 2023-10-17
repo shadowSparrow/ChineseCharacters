e44017a3046f154d80907f7fd16882693b7b690c
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-10-17 17:53:41 +0200

    UI for detailView

--- ChineseCharacters/ViewController.swift
+++ ChineseCharacters/ViewController.swift
@@ -9,35 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var characterLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: self.view.center.x, y: self.view.center.y, width: 190, height: 150))
-        label.center.x = self.view.center.x
-        label.center.y = self.view.center.y
-        label.layer.masksToBounds = true
-        label.layer.cornerRadius = 5
+    private let characterLabel: UILabel = {
+        let label = UILabel()
         
-        label.textColor = .white
-        label.backgroundColor = .blue
-        label.textAlignment = .center
-        label.text = "Character"
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private lazy var readingLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: self.view.center.x, y: self.view.center.y, width: 190, height: 50))
-        label.center.x = self.view.center.x
-        label.center.y = self.view.center.y + 104
         label.layer.masksToBounds = true
         label.layer.cornerRadius = 5
         
         label.textColor = .white
         label.backgroundColor = .red
         label.textAlignment = .center
-        label.text = "PingYing"
-        //label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 120.0)
+        //label.font = UIFont(name: "FZKai-Z03S", size: 120)
+        label.text = ""
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    private let readingLabel: UILabel = {
+        let label = UILabel()
+        
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 5
+        label.textColor = .white
+        label.backgroundColor = .red
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 36.0)
+        label.text = ""
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+     private let characterView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+         view.layer.cornerRadius = 5
+         view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //MARK: TODO
+    private let strokesString: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .red
+        label.textColor = .white
+        label.text = "strokes"
+        
+        return label
+    }()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,9 +71,42 @@ class ViewController: UIViewController {
     
     //MARK: CreateUI
     
-    func setUIElements(){
-        self.view.addSubview(characterLabel)
-        self.view.addSubview(readingLabel)
+    func setUIElements() {
+        self.view.addSubview(characterView)
+        
+        
+        
+        characterView.addSubview(characterLabel)
+        characterView.addSubview(readingLabel)
+        
+        //setConstraints
+        
+        let screenWindth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        NSLayoutConstraint.activate([
+            characterView.widthAnchor.constraint(equalToConstant:  screenWindth/1.5),
+            characterView.heightAnchor.constraint(equalToConstant: screenHeight/2.5),
+            characterView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            characterView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+            
+            characterLabel.widthAnchor.constraint(equalToConstant: 200),
+            characterLabel.heightAnchor.constraint(equalToConstant: 150),
+                                                   
+            characterLabel.centerXAnchor.constraint(equalTo: characterView.centerXAnchor),
+            characterLabel.topAnchor.constraint(equalTo: characterView.topAnchor),
+            
+            readingLabel.widthAnchor.constraint(equalToConstant: 200),
+            readingLabel.heightAnchor.constraint(equalToConstant: 50),
+            readingLabel.centerXAnchor.constraint(equalTo: characterView.centerXAnchor),
+            readingLabel.topAnchor.constraint(equalTo: characterLabel.bottomAnchor),
+            
+            
+        ])
+        
+        
+        
+        //self.view.addSubview(characterLabel)
+        //self.view.addSubview(readingLabel)
     }
     
     //MARK: Networking
